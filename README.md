6c62700add369a41d34892299ded5862d2b0327b
Dean <dean@N510.(none)>
2012-07-04 20:41:43 +0100

    updated to v0.2

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-Affililink
-==========
+# Affililink
 
 A free way to automate your affiliate scheme.
 Just add your affiliate codes to the top of the script then link to it from your theme.
 
 Full details at http://affililink.com/.
+
+## Changelog
+### v0.1 7th May 2012
+First release, basic working version with Amazon and eBay.
+
+### v0.2 4th July 2012
+Reshuffled code, created a new function for each affiliate scheme.
+Track clicked links with Google Analytics (Event: Affililink > Site > URL).
+Added support for Javari (UK, DE, FR), Amazon Supply, Amazon Wireless, Endless.
+
+## TODO
+
+* Create addTagToEnd universal function (for amazon style tags)
+* Add replace existing tag option
+* Add replace rover.ebay.com tag functionality
+* Log analytics views as well as clicks
+* Add function to rewrite individual links (which are loaded by ajax after page has loaded)
+* Create script to test links
--- affililink.js
+++ affililink.js
@@ -2,30 +2,133 @@
 *  Affililink v0.2
 *  http://affililink.com
 *  Created by Dean Barrow (http://deanbarrow.co.uk)
-*
-*  Changelog
-*  v0.1 First release
-*  v0.2 Added Javari support
 */
 var affililink, curronload, newonload;
 affililink = function() {
+
   /* enter your affiliate codes below */
-  var a, amazon, amazon_domain, amazon_domains, domain, ebay, ebay_domain, ebay_domains, host, url, _i, _j, _k, _len, _len2, _len3, _results;
-  amazon = {
+  var a, amazon, amazon_code, domain, ebay, ebay_code, host, track, tracking_code, url, _i, _len, _results;
+  amazon_code = {
     'amazon.co.uk': '',
     'amazon.com': '',
     'amazon.de': '',
     'amazon.fr': '',
     'javari.co.uk': '',
     'javari.de': '',
-    'javari.fr': ''
+    'javari.fr': '',
+    'amazonsupply.com': '',
+    'amazonwireless.com': '',
+    'endless.com': ''
   };
-  ebay = {
+  ebay_code = {
     'campaign': 0,
     'country': ''
-    /* find all A tags */
   };
-  a = document.getElementsByTagName("a");
+  
+  
+  /* google analytics tracking */
+  tracking_code = {
+    'ga': false
+  };
+  
+  
+  /* DO NOT EDIT BELOW THIS LINE */
+  /* track click analytics */
+  track = function(url, site) {
+    if (tracking_code['ga']) {
+      url.setAttribute('onclick', "_gaq.push(['_trackEvent', 'Affililink', " + site + ", " + url.href + "]);");
+    }
+    return true;
+  };
+  
+  /* ebay */
+  ebay = function(url) {
+    var ebay_domain, ebay_domains, _i, _len;
+    if (ebay_code['campaign'] && ebay_code['country']) {
+      ebay_domains = ['ebay.com.au', 'ebay.at', 'ebay.be', 'ebay.ca', 'ebay.ch', 'ebay.de', 'ebay.es', 'ebayanuncios.es', 'ebay.fr', 'ebay.ie', 'ebay.it', 'ebay.nl', 'ebay.co.uk', 'ebay.com', 'half.com'];
+      for (_i = 0, _len = ebay_domains.length; _i < _len; _i++) {
+        ebay_domain = ebay_domains[_i];
+        if (!(domain === ebay_domain || domain.substring(domain.length - ebay_domain.length - 1) === '.' + ebay_domain && domain !== 'rover.ebay.com')) {
+          continue;
+        }
+        switch (ebay_code['country']) {
+          case 'AT':
+            ebay_code['code'] = '5221-53469-19255-0';
+            break;
+          case 'AU':
+            ebay_code['code'] = '705-53470-19255-0';
+            break;
+          case 'BE':
+            ebay_code['code'] = '1553-53471-19255-0';
+            break;
+          case 'CA':
+            ebay_code['code'] = '706-53473-19255-0';
+            break;
+          case 'CH':
+            ebay_code['code'] = '5222-53480-19255-0';
+            break;
+          case 'DE':
+            ebay_code['code'] = '707-53477-19255-0';
+            break;
+          case 'ES':
+            ebay_code['code'] = '1185-53479-19255-0';
+            break;
+          case 'FR':
+            ebay_code['code'] = '709-53476-19255-0';
+            break;
+          case 'IE':
+            ebay_code['code'] = '5282-53468-19255-0';
+            break;
+          case 'IT':
+            ebay_code['code'] = '724-53478-19255-0';
+            break;
+          case 'NL':
+            ebay_code['code'] = '1346-53482-19255-0';
+            break;
+          case 'UK':
+            ebay_code['code'] = '710-53481-19255-0';
+            break;
+          case 'US':
+            ebay_code['code'] = '711-53200-19255-0';
+        }
+        if (domain.substring(domain.length - 'half.com'.length) === 'half.com') {
+          url.href = 'http://rover.ebay.com/rover/1/8971-56017-19255-0/1?ff3=8&pub=5574962087&toolid=10001&campid=' + ebay_code['campaign'] + '&customid=affililink&mpre=' + encodeURIComponent(url.href);
+        } else {
+          url.href = 'http://rover.ebay.com/rover/1/' + ebay_code['code'] + '/1?ff3=4&pub=5574962087&toolid=10001&campid=' + ebay_code['campaign'] + '&customid=affililink&mpre=' + encodeURIComponent(url.href);
+        }
+        track(url, 'eBay');
+        return true;
+      }
+    }
+  };
+  
+  /* amazon & javari */
+  amazon = function(url) {
+    var amazon_domain, amazon_domains, _i, _len;
+    amazon_domains = ['amazon.co.uk', 'amazon.com', 'amazon.de', 'amazon.fr', 'javari.co.uk', 'javari.de', 'javari.fr', 'amazonsupply.com', 'amazonwireless.com', 'endless.com'];
+    for (_i = 0, _len = amazon_domains.length; _i < _len; _i++) {
+      amazon_domain = amazon_domains[_i];
+      if (!(domain === amazon_domain || domain.substring(domain.length - amazon_domain.length - 1) === '.' + amazon_domain)) {
+        continue;
+      }
+      if (amazon_code[amazon_domain]) {
+        url.href = url.href.replace(/tag=([a-z0-9\-]+)/g, '');
+        url.href = url.href.replace('&&', '&');
+        if (url.href.substring(-1, 1) === '/') {
+          url.href = url.href.substring(0, url.href.length - 1);
+        }
+        if (!url.href.split("/")[3]) {
+          url.href += '?tag=' + amazon_code[amazon_domain];
+        } else {
+          url.href += '&tag=' + amazon_code[amazon_domain];
+        }
+        track(url, 'Amazon');
+        return true;
+      }
+    }
+  };
+  /* find all A tags */
+  a = document.getElementsByTagName('a');
   host = window.location.hostname;
   /* loop through links */
   _results = [];
@@ -39,85 +142,13 @@ affililink = function() {
     if (!domain) {
       continue;
     } else {
-      /* ebay */
-      if (ebay['campaign'] && ebay['country']) {
-        ebay_domains = ['ebay.com.au', 'ebay.at', 'ebay.be', 'ebay.ca', 'ebay.ch', 'ebay.de', 'ebay.es', 'ebayanuncios.es', 'ebay.fr', 'ebay.ie', 'ebay.it', 'ebay.nl', 'ebay.co.uk', 'ebay.com', 'half.com'];
-        for (_j = 0, _len2 = ebay_domains.length; _j < _len2; _j++) {
-          ebay_domain = ebay_domains[_j];
-          if (!(domain === ebay_domain || domain.substring(domain.length - ebay_domain.length - 1) === '.' + ebay_domain && domain !== 'rover.ebay.com')) {
-            continue;
-          }
-          switch (ebay['country']) {
-            case 'AT':
-              ebay['code'] = '5221-53469-19255-0';
-              break;
-            case 'AU':
-              ebay['code'] = '705-53470-19255-0';
-              break;
-            case 'BE':
-              ebay['code'] = '1553-53471-19255-0';
-              break;
-            case 'CA':
-              ebay['code'] = '706-53473-19255-0';
-              break;
-            case 'CH':
-              ebay['code'] = '5222-53480-19255-0';
-              break;
-            case 'DE':
-              ebay['code'] = '707-53477-19255-0';
-              break;
-            case 'ES':
-              ebay['code'] = '1185-53479-19255-0';
-              break;
-            case 'FR':
-              ebay['code'] = '709-53476-19255-0';
-              break;
-            case 'IE':
-              ebay['code'] = '5282-53468-19255-0';
-              break;
-            case 'IT':
-              ebay['code'] = '724-53478-19255-0';
-              break;
-            case 'NL':
-              ebay['code'] = '1346-53482-19255-0';
-              break;
-            case 'UK':
-              ebay['code'] = '710-53481-19255-0';
-              break;
-            case 'US':
-              ebay['code'] = '711-53200-19255-0';
-          }
-          if (domain.substring(domain.length - 'half.com'.length) === 'half.com') {
-            url.href = 'http://rover.ebay.com/rover/1/8971-56017-19255-0/1?ff3=8&pub=5574962087&toolid=10001&campid=' + ebay['campaign'] + '&customid=affililink&mpre=' + encodeURIComponent(url.href);
-          } else {
-            url.href = 'http://rover.ebay.com/rover/1/' + ebay['code'] + '/1?ff3=4&pub=5574962087&toolid=10001&campid=' + ebay['campaign'] + '&customid=affililink&mpre=' + encodeURIComponent(url.href);
-          }
-        }
-      }
-      /* amazon & javari */
-      amazon_domains = ['amazon.co.uk', 'amazon.com', 'amazon.de', 'amazon.fr', 'javari.co.uk', 'javari.de', 'javari.fr'];
-      for (_k = 0, _len3 = amazon_domains.length; _k < _len3; _k++) {
-        amazon_domain = amazon_domains[_k];
-        if (!(domain === amazon_domain || domain.substring(domain.length - amazon_domain.length - 1) === '.' + amazon_domain)) {
-          continue;
-        }
-        if (amazon[amazon_domain]) {
-          url.href = url.href.replace(/tag=([a-z0-9\-]+)/g, '');
-        }
-        url.href = url.href.replace('&&', '&');
-        if (url.href.substring(-1, 1) === '/') {
-          url.href = url.href.substring(0, url.href.length - 1);
-        }
-        if (!url.href.split("/")[3]) {
-          url.href += '?tag=' + amazon[amazon_domain];
-        } else {
-          url.href += '&tag=' + amazon[amazon_domain];
-        }
-      }
+      amazon(url);
+      ebay(url);
     }
   }
   return _results;
 };
+
 /* only run when page has loaded */
 if (window.attachEvent) {
   window.attachEvent("onload", affililink);
